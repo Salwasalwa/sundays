0a476f912e8a48b0b49e809586154d6a0ec20dc8
salwa <salwaoudad@gmail.com>
2017-03-22 14:31:29 +0100

    add contact.html ---> improve this js & css

--- static/js/script.js
+++ static/js/script.js
@@ -19,4 +19,69 @@ jQuery(document).ready(function($) {
                var id = $('.item.active').data('slide-number');
               $('#carousel-text').html($('#slide-content-'+id).html());
       });
+
+      $('#contactForm').bootstrapValidator({
+          container: '#messages',
+          feedbackIcons: {
+              valid: 'glyphicon glyphicon-ok',
+              invalid: 'glyphicon glyphicon-remove',
+              validating: 'glyphicon glyphicon-refresh'
+          },
+          fields: {
+              fullName: {
+                  validators: {
+                      notEmpty: {
+                          message: 'The full name is required and cannot be empty'
+                      },
+                      stringLength: {
+                          min: 5,
+                          message: 'le nom doit avoir au moins 5 caractères'
+                      }
+                  }
+              },
+              email: {
+                  validators: {
+                      notEmpty: {
+                          message: 'The email address is required and cannot be empty'
+                      },
+                      emailAddress: {
+                          message: 'The email address is not valid'
+                      }
+                  }
+              },
+              title: {
+                  validators: {
+                      notEmpty: {
+                          message: 'The title is required and cannot be empty'
+                      },
+                      stringLength: {
+                          max: 100,
+                          message: 'The title must be less than 100 characters long'
+                      }
+                  }
+              },
+              content: {
+                  validators: {
+                      notEmpty: {
+                          message: 'The content is required and cannot be empty'
+                      },
+                      stringLength: {
+                          max: 500,
+                          message: 'The content must be less than 500 characters long'
+                      }
+                  }
+              }
+          }
+      });
+
+      "use strict";
+      var options = {};
+      options.ui = {
+          container: "#pwd-container",
+          showVerdictsInsideProgressBar: true,
+          viewports: {
+              progress: ".progress_meter"
+          }
+      };
+      $('#password').pwstrength(options);
 });
